a037d1fb82d9d07be61c9b50c8aaac88574d02f8
s109178 <92769859+s109178@users.noreply.github.com>
2024-01-03 16:10:52 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>顏仁駿Python網頁12/6</h1>"
+    homepage = "<h1>顏仁駿Python網頁1/3a</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=>傳送使用者暱稱</a><br>"
@@ -31,6 +31,27 @@ def index():
     homepage += "<br><a href=/webhookfinalinebot></a><br>"
     return homepage
 
+
+@app.route("/webhookfinalinebot", methods=["POST"])
+def webhookfinalinebot():
+    req = request.get_json(force=True)
+    action = req.get("queryResult").get("action")
+    info = action
+    #if (action == "zodiaChoice"):
+        #ZodiacSigns = req.get("queryResult").get("parameters").get("ZodiacSigns")
+        #info = "您選擇的星座是:" + ZodiacSigns +"，星座運勢：\n"
+
+        #db = firestore.client()
+        #collection_ref = db.collection("星座")
+        #docs = collection_ref.get()
+        #result = ""
+        #for doc in docs:
+            #dict = doc.to_dict()
+            #if ZodiacSigns in dict["ZodiacSigns"]:
+                #result += "星座名：" + dict["title"] + "\n"
+                #result += "運勢：" + dict["hyperlink"] + "\n\n"
+        #info += result
+    return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
     app.run(debug=True)
